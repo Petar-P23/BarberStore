2bf4f4132e4ff75c10a6ddab7d082bb96e6aea95
Petar Prashtilov <petio2003@students.softuni.bg>
2022-03-21 18:13:36 +0200

    Exception constants added

---
+++ BarberStore.Core/Constants/ExceptionMessageConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace BarberStore.Core.Constants;
+
+public static class ExceptionMessageConstants
+{
+    public const string GuardNullException = "{0} cannot be null.";
+    public const string GuardStringException = "{0} cannot is empty or missing.";
+}
