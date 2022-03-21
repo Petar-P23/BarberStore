ca2f1daccac6d0da6484c48ec3254078c1426954
Petar Prashtilov <petio2003@students.softuni.bg>
2022-03-21 18:13:23 +0200

    Guard class added

---
+++ BarberStore.Core/Common/Guard.cs
@@ -0,0 +1,24 @@
+﻿using static BarberStore.Core.Constants.ExceptionMessageConstants;
+
+namespace BarberStore.Core.Common;
+
+public static class Guard
+{
+    public static void AgainstNull(object? value, string? name = null)
+    {
+        name ??= nameof(value);
+        if (value == null)
+        {
+            throw new ArgumentNullException(string.Format(GuardNullException, name));
+        }
+    }
+
+    public static void AgainstNullOrWhiteSpaceString(string? value, string? name = null)
+    {
+        name ??= nameof(value);
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentNullException(string.Format(GuardStringException, name));
+        }
+    }
+}
