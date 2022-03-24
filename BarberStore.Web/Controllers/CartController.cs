902feea2f89760133dbe94569397ff3ddc6dd5f3
Petar Prashtilov <petio2003@students.softuni.bg>
2022-03-24 11:45:34 +0200

    Extended controller functionality

--- BarberStore.Web/Controllers/CartController.cs
+++ BarberStore.Web/Controllers/CartController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq.Expressions;
+using BarberStore.Core.Contracts;
+using BarberStore.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BarberStore.Web.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly IStoreService storeService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CartController(IStoreService storeService,
+            UserManager<ApplicationUser> userManager)
         {
-            return this.View();
+            this.storeService = storeService;
+            this.userManager = userManager;
         }
+
+        public async Task<IActionResult> GetCart()
+        {
+            try
+            {
+                var user = this.userManager.GetUserId(this.User);
+                var cart = await this.storeService.GetCart(user);
+                return this.Ok(cart);
+            }
+            catch (Exception)
+            {
+                return base.NotFound();
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProductToCart(string productId, int quantity)
+        {
+                var user = this.userManager.GetUserId(this.User);
+                var (success,errors) = await this.storeService.AddProductToCart(user, productId, quantity);
+
+                if (success)
+                    return this.Ok();
+                else
+                    return base.BadRequest(errors);
+        }
+
+        
     }
 }
--- BarberStore.Web/Controllers/StoreController.cs
+++ BarberStore.Web/Controllers/StoreController.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BarberStore.Core.Contracts;
+using BarberStore.Core.Models.Store;
+using BarberStore.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BarberStore.Web.Controllers
 {
     public class StoreController : Controller
     {
-        public IActionResult Index()
+        private const int DefaultPageSize = 20;
+        private const int DefaultPage = 1;
+        private readonly IStoreService storeService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public StoreController(IStoreService storeService,
+               UserManager<ApplicationUser> userManager)
         {
-            return this.View();
+            this.storeService = storeService;
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Explore(int page, int size, string category)
+        {
+            if (size == 0) size = DefaultPageSize;
+            if (page == 0) page = DefaultPage;
+
+            var products = await this.storeService.GetStorePage(page, size, category);
+            return this.View(products);
+        }
+        public async Task<IActionResult> Product(string id, string? errors)
+        {
+            if (errors != null)
+            {
+                this.ViewBag["error"] = errors;
+            }
+
+            var product = await this.storeService.GetProductPage(id);
+
+            return this.View(product);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddProductToCart(string productId, int quantity)
+        {
+            if (quantity == 0) quantity = 1;
+            var user = this.userManager.GetUserId(this.User);
+            var (success, errors) = await this.storeService.AddProductToCart(user, productId, quantity);
+            if (success)
+                return this.Ok();
+
+            return this.RedirectToAction("Product", routeValues: new { productId, errors });
+        }
+        [Authorize]
+        public async Task<IActionResult> PlaceOrder()
+        {
+            var user = this.userManager.GetUserId(this.User);
+
+            var order = await this.storeService.GetCart(user);
+            if (order == null) 
+                return this.RedirectToAction("Explore");
+
+            return this.View(order);
+        }
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> PlaceOrder(PlaceOrderModel orderModel)
+        {
+            var (success, errors) = await this.storeService.PlaceOrder(orderModel);
+            if (!success)
+            {
+                this.ViewBag["error"] = errors;
+                return this.View();
+            }
+
+            return this.View("SuccessfulOrder");
         }
     }
 }
